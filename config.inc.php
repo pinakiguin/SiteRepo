2f99e2cf8699f1e4e4e624ce6c2d420db586c253
abusalam <abusalamparvezalam@gmail.com>
2013-07-17 07:14:03 +0530

    Added New AppKey

--- config.inc.php
+++ config.inc.php
@@ -7,39 +7,46 @@
 /**
  * Defines MySQL Server Hostname
  */
-define("HOST_Name", "localhost");
+define('HOST_Name', 'localhost');
 /**
  * Defines MySQL Server Username
  */
-define("MySQL_User", "root");
+define('MySQL_User', 'root');
 /**
  * Defines MySQL Server password
  */
-define("MySQL_Pass", "mysql");
+define('MySQL_Pass', 'mysql');
 /**
  * Defines MySQL Server DB Name
  */
-define("MySQL_DB", "WebSite");
+define('MySQL_DB', 'WebSite');
 /**
  * Defines MySQL Server Hostname
  */
-define("MySQL_Pre", "WebSite_");
+define('MySQL_Pre', 'WebSite_');
 /**
  * Defines Session Timeout value in minutes
  */
-define("LifeTime", "20");
+define('LifeTime', '20');
 /**
  * Defines path after Hostname of the script to generate menu links
  */
-define("BaseDIR", "/SiteRepo/");
+define('BaseDIR', '/SiteRepo/');
 /**
  * Defines Title of the page
  */
-define("AppTitle", "Paschim Medinipur");
+define('AppTitle', 'Paschim Medinipur');
+/**
+ * Defines Application's Unique Installation ID
+ *
+ * 1. Validates Against Multiple sessions
+ * 2. Used to Encrypt Passwords in Database
+ */
+define('AppKey', 'e$#KH+)&Q5&CP3798k7$quyxGfR48+vX');
 /**
  * Defines Existence of tables
  *
  * If set to TRUE then Databse will be created
  */
-define("NeedsDB", TRUE);
+define('NeedsDB', TRUE);
 ?>
