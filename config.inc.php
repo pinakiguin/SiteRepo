5ae60e73960642505afe450127ca62e302cb0eed
abusalam <abusalamparvezalam@gmail.com>
2013-07-20 08:45:10 +0530

    Added Flag for SMS Gateway

--- config.inc.php
+++ config.inc.php
@@ -8,34 +8,42 @@
  * Defines MySQL Server Hostname
  */
 define('HOST_Name', 'localhost');
+
 /**
  * Defines MySQL Server Username
  */
 define('MySQL_User', 'root');
+
 /**
  * Defines MySQL Server password
  */
 define('MySQL_Pass', 'mysql');
+
 /**
  * Defines MySQL Server DB Name
  */
 define('MySQL_DB', 'WebSite');
+
 /**
  * Defines MySQL Server Hostname
  */
 define('MySQL_Pre', 'WebSite_');
+
 /**
  * Defines Session Timeout value in minutes
  */
 define('LifeTime', '20');
+
 /**
  * Defines path after Hostname of the script to generate menu links
  */
 define('BaseDIR', '/SiteRepo/');
+
 /**
  * Defines Title of the page
  */
 define('AppTitle', 'Paschim Medinipur');
+
 /**
  * Defines Application's Unique Installation ID
  *
@@ -43,10 +51,18 @@ define('AppTitle', 'Paschim Medinipur');
  * 2. Used to Encrypt Passwords in Database
  */
 define('AppKey', 'e$#KH+)&Q5&CP3798k7$quyxGfR48+vX');
+
 /**
  * Defines Existence of tables
  *
  * If set to TRUE then Databse will be created
  */
 define('NeedsDB', TRUE);
+
+/**
+ * Defines use of SMS Gateway
+ *
+ * If set to TRUE then SMS will be Sent
+ */
+define('UseSMSGW', FALSE);
 ?>
