5d9ae39afd56963bf6302bc81942043fce0abccd
abusalam <abusalamparvezalam@gmail.com>
2013-12-28 16:08:42 +0530

    MPR WIP

--- mpr/Department.php
+++ mpr/Department.php
@@ -16,12 +16,33 @@ if (NeedsDB) {
     <h1><?php echo AppTitle; ?></h1>
   </div>
   <div class="Header"></div>
+  <form method="post" action="<?php echo WebLib::GetVal($_SERVER, 'PHP_SELF'); ?>">
+    <div>
+      <div style="height: 200px;overflow-y: scroll;float: left;border:1px solid;">
+        <ul>
+          <label>Name of Department</label>
+          <input textbox name ="txtdeptname" id="txtdeptname"/>
+          <input type="submit" id="Cmdsub" name="Cmdsub">
+        </ul>
+      </div>
+  </form>
+
   <?php
   WebLib::ShowMenuBar('MPR');
   ?>
   <div class="content">
+    <?php
+    $Data = new MySQLiDBHelper();
+    $DataACL['UserMapID'] = $_SESSION['UserMapID'];
+    $DataACL['DeptName'] = $_POST['DeptName'];
+    $Data->insert(MySQL_Pre . 'Departments', $DataACL);
+    //echo'Add';
+    $_SESSION['Msg'] = 'Add Successfully!';
+    ?>
 
   </div>
+
+
   <div class="pageinfo">
     <?php WebLib::PageInfo(); ?>
   </div>
--- mpr/sql.defs.php
+++ mpr/sql.defs.php
@@ -13,16 +13,19 @@ function SQLDefs($ObjectName) {
       break;
     case 'MPR_Sectors':
       $SqlDB = 'CREATE TABLE IF NOT EXISTS `' . MySQL_Pre . $ObjectName . '` ('
-              . '`DeptID` bigint(20) NOT NULL AUTO_INCREMENT,'
-              . '`DeptName` VARCHAR(100) DEFAULT NULL,'
-              . ' PRIMARY KEY (`DeptID`)'
+              . '`SectorID` bigint(20) NOT NULL AUTO_INCREMENT,'
+              . '`SectorName` VARCHAR(100) DEFAULT NULL,'
+              . '`UserMapID` INT(10) DEFAULT 1,'
+              . ' PRIMARY KEY (`SectorID`)'
               . ') ENGINE=InnoDB DEFAULT CHARSET=utf8;';
       break;
     case 'MPR_Schemes':
       $SqlDB = 'CREATE TABLE IF NOT EXISTS `' . MySQL_Pre . $ObjectName . '` ('
-              . '`DeptID` bigint(20) NOT NULL AUTO_INCREMENT,'
-              . '`DeptName` VARCHAR(100) DEFAULT NULL,'
-              . ' PRIMARY KEY (`DeptID`)'
+              . '`SchemeID` bigint(20) NOT NULL AUTO_INCREMENT,'
+              . '`schemeName` VARCHAR(100) DEFAULT NULL,'
+              . '`DeptID` INT(10),'
+              . '`SectorID` INT(10),'
+              . ' PRIMARY KEY (`SchemeID`)'
               . ') ENGINE=InnoDB DEFAULT CHARSET=utf8;';
       break;
     case 'MPR_Projects':
@@ -32,11 +35,29 @@ function SQLDefs($ObjectName) {
               . ' PRIMARY KEY (`DeptID`)'
               . ') ENGINE=InnoDB DEFAULT CHARSET=utf8;';
       break;
+    case 'MPR_Projects':
+      $SqlDB = 'CREATE TABLE IF NOT EXISTS `' . MySQL_Pre . $ObjectName . '` ('
+              . '`ProjectID` bigint(20) NOT NULL AUTO_INCREMENT,'
+              . '`SchemeID` INT(10),'
+              . '`ProjectName` VARCHAR(100) DEFAULT NULL,'
+              . '`ProjectCost` INT(10),'
+              . '`StartDate` date,'
+              . '`AlotmentAmount` INT(10),'
+              . '`AlotmentDate` date,'
+              . '`TenderDate` date,'
+              . '`WorkOrderDate` date,'
+              . ' PRIMARY KEY (`ProjectID`)'
+              . ') ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+      break;
     case 'MPR_Progress':
       $SqlDB = 'CREATE TABLE IF NOT EXISTS `' . MySQL_Pre . $ObjectName . '` ('
-              . '`DeptID` bigint(20) NOT NULL AUTO_INCREMENT,'
-              . '`DeptName` VARCHAR(100) DEFAULT NULL,'
-              . ' PRIMARY KEY (`DeptID`)'
+              . '`ReportID` bigint(20) NOT NULL AUTO_INCREMENT,'
+              . '`ReportDate` date,'
+              . '`ProjectID` INT(10),'
+              . '`PhysicalProgress` VARCHAR(100) DEFAULT NULL,'
+              . '`FinancialProgress` VARCHAR(100) DEFAULT NULL,'
+              . '`Remarks` VARCHAR(300) DEFAULT NULL,'
+              . ' PRIMARY KEY (`ReportID`)'
               . ') ENGINE=InnoDB DEFAULT CHARSET=utf8;';
       break;
     case 'MPR_UserMaps':
