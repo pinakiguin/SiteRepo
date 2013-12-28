6d55e4ec84956a7fad654834515edceb6c0905e4
abusalam <abusalamparvezalam@gmail.com>
2013-12-28 11:45:10 +0530

    RSBY WIP

--- rsby/js/Data.js
+++ rsby/js/Data.js
@@ -27,4 +27,28 @@ $(function() {
   });
 
   $('input[type="submit"]').button();
+  /**
+   * Get the list of ACs and register change event to update Parts
+   * @todo ACNo Change event registered
+   *
+   */
+  $('#ACNo')
+          .chosen({width: "300px",
+    no_results_text: "Oops, nothing found!"
+  })
+          .change(function() {
+    var Options = '<option value=""></option>';
+    var Parts = $('#PartID').data('Parts');
+    var ACNo = $('#ACNo').val();
+    $.each(Parts.Data,
+            function(index, value) {
+              if (value.ACNo === ACNo) {
+                Options += '<option value="' + value.PartID + '">'
+                        + value.PartNo + ' - ' + value.PartName
+                        + '</option>';
+              }
+            });
+    $('#PartID').html(Options)
+            .trigger("liszt:updated");
+  });
 });
