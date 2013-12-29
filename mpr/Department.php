e37e828611a4994cf1a72baf8ea994804c602ff0
abusalam <abusalamparvezalam@gmail.com>
2013-12-30 00:09:59 +0530

    Beautification of MPR Department Form

--- mpr/Department.php
+++ mpr/Department.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/../lib.inc.php';
 WebLib::AuthSession();
 WebLib::Html5Header('Departments');
 WebLib::IncludeCSS();
-if (NeedsDB) {
-  WebLib::CreateDB('MPR');
-}
+WebLib::JQueryInclude();
+WebLib::IncludeJS('mpr/js/forms.js');
+WebLib::IncludeCSS('mpr/css/forms.css');
 ?>
 </head>
 <body>
@@ -16,33 +16,31 @@ if (NeedsDB) {
     <h1><?php echo AppTitle; ?></h1>
   </div>
   <div class="Header"></div>
-  <form method="post" action="<?php echo WebLib::GetVal($_SERVER, 'PHP_SELF'); ?>">
-    <div>
-      <div style="height: 200px;overflow-y: scroll;float: left;border:1px solid;">
-        <ul>
-          <label>Name of Department</label>
-          <input textbox name ="txtdeptname" id="txtdeptname"/>
-          <input type="submit" id="Cmdsub" name="Cmdsub">
-        </ul>
-      </div>
-  </form>
-
   <?php
   WebLib::ShowMenuBar('MPR');
   ?>
   <div class="content">
-    <?php
-    $Data = new MySQLiDBHelper();
-    $DataACL['UserMapID'] = $_SESSION['UserMapID'];
-    $DataACL['DeptName'] = $_POST['DeptName'];
-    $Data->insert(MySQL_Pre . 'Departments', $DataACL);
-    //echo'Add';
-    $_SESSION['Msg'] = 'Add Successfully!';
-    ?>
-
+    <div class="formWrapper">
+      <form method="post" action="<?php echo WebLib::GetVal($_SERVER, 'PHP_SELF'); ?>">
+        <h3>Departments</h3>
+        <label for="txtdeptname"><strong>Name of Department</strong></label>
+        <input type="text" name ="txtdeptname" id="txtdeptname" placeholder="Name of Department"/>
+        <?php
+        $Data = new MySQLiDBHelper();
+        if (WebLib::GetVal($_POST, 'txtdeptname') !== null) {
+          $DataACL['UserMapID'] = $_SESSION['UserMapID'];
+          $DataACL['DeptName'] = WebLib::GetVal($_POST, 'txtdeptname');
+          $Data->insert(MySQL_Pre . 'MPR_Departments', $DataACL);
+          echo'Add Successfully';
+          $_SESSION['Msg'] = 'Add Successfully!';
+        }
+        ?>
+        <div class="formControl">
+          <input type="submit" id="Cmdsub" name="Cmdsub">
+        </div>
+      </form>
+    </div>
   </div>
-
-
   <div class="pageinfo">
     <?php WebLib::PageInfo(); ?>
   </div>
