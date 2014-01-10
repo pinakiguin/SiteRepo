48b37a422898439bb93854bb438dc806d168c9de
Suman Pal <sumanpalsumanpal@gmail.com>
2014-01-10 14:32:18 +0530

    Format PP2 WIP

---
+++ pp/js/forms.js
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function() {
+  $('input[type="submit"]').button();
+
+  $('.chzn-select').chosen({width: "250px",
+    no_results_text: "Oops, nothing found!"
+  });
+
+  $('#DOB').datepicker({
+    showOn: "both",
+    buttonImage: "images/calendar.gif",
+    buttonImageOnly: true
+  });
+
+});
