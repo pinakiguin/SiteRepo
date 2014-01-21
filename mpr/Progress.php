12d5489f67ac9cf23ea848a3e769f3c63ba9c168
Suman Pal <sumanpalsumanpal@gmail.com>
2014-01-21 11:32:11 +0530

    some code formatting

--- mpr/Progress.php
+++ mpr/Progress.php
@@ -20,73 +20,77 @@ WebLib::IncludeJS('js/chosen.jquery.min.js');
     <div class="RightPanelSide"></div>
     <h1><?php echo AppTitle; ?></h1>
   </div>
-  <div class="Header">
-
-  </div>
+  <div class="Header"></div>
   <?php
   WebLib::ShowMenuBar('MPR');
   ?>
-
   <div class="content">
     <div class="formWrapper">
-      <form method="post" action="<?php echo WebLib::GetVal($_SERVER, 'PHP_SELF'); ?>">
+      <form method="post"
+            action="<?php
+            echo WebLib::GetVal($_SERVER, 'PHP_SELF');
+            ?>">
         <h3>Process </h3>
         <?php
         include __DIR__ . '/DataMPR.php';
-        $Data = new MySQLiDB();
+        WebLib::ShowMsg();
+        $Data  = new MySQLiDB();
         $Data1 = new MySQLiDBHelper();
         ?>
         <div class="FieldGroup">
-          <label for="ProjectName"><strong>Project Name</strong></label><br/>
-          <select name="ProjectID" data-placeholder="Select Project">
-            <?php
-            $Query = 'Select `ProjectID`, `ProjectName` '
-                    . ' FROM `' . MySQL_Pre . 'MPR_Projects` '
-                    . ' Order By `ProjectID`';
-            $Data->show_sel('ProjectID', 'ProjectName', $Query, WebLib::GetVal($_POST, 'ProjectID'));
-            ?>
-          </select>
+          <label for="ProjectName">
+            <strong>Project Name</strong>
+            <select name="ProjectID" data-placeholder="Select Project">
+              <?php
+              $Query = 'Select `ProjectID`, `ProjectName` '
+                  . ' FROM `' . MySQL_Pre . 'MPR_Projects` '
+                  . ' Order By `ProjectID`';
+              $Data->show_sel('ProjectID', 'ProjectName', $Query,
+                              WebLib::GetVal($_POST, 'ProjectID'));
+              ?>
+            </select>
+          </label>
         </div>
-
-
         <div class="FieldGroup">
-          <label for="ReportDate"><strong>Report Date</strong></label>
-
-          <input type="text" id="ReportDate" name="ReportDate"
-                 class="DatePicker" placeholder="YYYY-MM-DD" />
+          <label for="ReportDate">
+            <strong>Report Date</strong>
+            <input type="text" id="ReportDate" name="ReportDate"
+                   class="DatePicker" placeholder="YYYY-MM-DD" />
+          </label>
         </div>
-
         <div class="FieldGroup">
-          <label for="PhysicalProgress"><strong>Physical Progress in %</strong></label>
-          <input type="text" name="PhysicalProgress" id="PhysicalProgress" placeholder="Physical Progress"/>
+          <label for="PhysicalProgress">
+            <strong>Physical Progress in %</strong>
+            <input type="text" name="PhysicalProgress" id="PhysicalProgress"
+                   placeholder="Physical Progress"/>
+          </label>
         </div>
-
         <div class="FieldGroup">
-          <label for="FinancialProgress"><strong>Financial Progress in %</strong></label>
-          <input type="text" name="FinancialProgress" id="FinancialProgress" placeholder="Financial Progress"/>
+          <label for="FinancialProgress">
+            <strong>Financial Progress in %</strong>
+            <input type="text" name="FinancialProgress" id="FinancialProgress"
+                   placeholder="Financial Progress"/>
+          </label>
         </div>
-
         <div class="FieldGroup">
-          <label for="Remarks"><strong>Remarks</strong></label>
-          <input type="text" name="Remarks" id="Remarks" placeholder="Remarks"/>
+          <label for="Remarks">
+            <strong>Remarks</strong>
+            <input type="text" name="Remarks" id="Remarks"
+                   placeholder="Remarks"/>
+          </label>
         </div>
         <div class="formControl">
-          <br/>
+          <input type="hidden" name="FormToken"
+                 value="<?php echo WebLib::GetVal($_SESSION, 'FormToken') ?>" />
           <input type="submit" name="CmdSubmit" value="Create Progress">
         </div>
-        <input type="hidden" name="FormToken"
-               value="<?php echo WebLib::GetVal($_SESSION, 'FormToken') ?>" />
       </form>
     </div>
     <?php
     unset($Data);
     unset($Data1);
-    WebLib::ShowMsg();
     ?>
-
   </div>
-
-
   <div class="pageinfo">
     <?php WebLib::PageInfo(); ?>
   </div>
