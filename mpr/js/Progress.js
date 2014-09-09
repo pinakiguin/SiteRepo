77f36e9ed87cec5f5961195a933d23f3a95d275e
Abu Salam Parvez Alam <abusalamparvezalam@gmail.com>
2014-09-09 09:44:12 +0530

    Working on Progress Slider

--- mpr/js/Progress.js
+++ mpr/js/Progress.js
@@ -7,6 +7,8 @@ $(function () {
     $("#Work").chosen({width: "300px",
         no_results_text: "Oops, nothing found!"
     }).change(function () {
+        $("#PhyPrgSlider").data("minVal",20)
+            .slider("option", "value",20);
         $.ajax({
             type: 'POST',
             url: 'AjaxData.php',
@@ -89,13 +91,22 @@ $(function () {
 
     $("#PhyPrgSlider").slider({
         range: "min",
-        value: 0,
+        value: $(this).data("minVal"),
         min: 0,
         max: 100,
         slide: function (event, ui) {
             $("#PhyPrgValue").val(ui.value);
             $("#PhyPrgLbl").html(ui.value);
+        },
+        stop: function( event, ui ) {
+            var minVal=$(this).data("minVal");
+            if(ui.value<minVal){
+                $(this).slider( "option", "value",minVal);
+                $("#PhyPrgValue").val(minVal);
+                $("#PhyPrgLbl").html(minVal);
+                return false;
+            }
         }
-    }).find(".ui-slider-range").css({ "background": "#729fcf"})
-      .find(".ui-slider-handle").css({ "border-color": "#729fcf"});
+    }).find(".ui-slider-range").css({ "background": "#00ff00"})
+      .find(".ui-slider-handle").css({ "border-color": "#00ff00"});
 });
