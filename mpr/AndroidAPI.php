29097bebb61527c09f1cbb2a7b1795e89dfac976
abusalam <abusalamparvezalam@gmail.com>
2015-02-25 08:09:12 +0530

    Implemented Schemes and Expiry

--- mpr/AndroidAPI.php
+++ mpr/AndroidAPI.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 
 require_once(__DIR__ . '/../lib.inc.php');
 
-$RT = time();
+$RT       = time();
 $json     = file_get_contents('php://input');
 $jsonData = json_decode($json);
 
@@ -39,18 +39,41 @@ exit();
  *               "ST":"Wed 20 Aug 08:31:23 PM"}
  *
  */
-
 class AndroidAPI {
   private $Req;
   private $Resp;
+  private $Expiry;
 
   function __construct($jsonData) {
     $this->Resp['ET'] = time();
     $this->Req        = $jsonData;
+    $this->Expiry     = null;
   }
 
   function __unset($name) {
     $this->sendResponse();
+  }
+
+  /**
+   * @return null
+   */
+  public function getExpiry() {
+    /**
+     * Important: Tells volley not to cache the response
+     */
+    if ($this->Expiry == null) {
+      $Expires = time() - 3600;
+    } else {
+      $Expires = time() + $this->Expiry;
+    }
+    return $Expires;
+  }
+
+  /**
+   * @param null $Expiry
+   */
+  public function setExpiry($Expiry) {
+    $this->Expiry = $Expiry;
   }
 
   function __destruct() {
@@ -60,91 +83,12 @@ class AndroidAPI {
   public function executeAPI() {
     switch ($this->Req->API) {
       /**
-       * Register User: Register User with Mobile No. to get the Secret Key for HOTP
-       *
-       * TODO Important: Store New Credentials in an Alternate field for validation against OTP
+       * User Schemes: Retrieve all the Schemes for the User
        *
        * Request:
-       *   JSONObject={"API":"RU",
-       *               "MDN":"9876543210"}
-       *
-       * Response:
-       *    JSONObject={"API":true,
-       *               "DB": // Unused till now
-       *               "MSG":"Key Sent to Mobile No. 9876543210",
-       *               "ET":2.0987,
-       *               "ST":"Wed 20 Aug 08:31:23 PM"}
-       *
-       */
-      case 'RU':
-        $DB = new MySQLiDBHelper();
-        $DB->where('MobileNo', $this->Req->MDN);
-        $Profile = $DB->query('Select UserName, Designation, eMailID FROM ' . MySQL_Pre . 'SMS_Users');
-        if (count($Profile) > 0) {
-          $AuthUser = new AuthOTP(1);
-          $AuthUser->deleteUser($this->Req->MDN);
-          $SecretKey = $AuthUser->setUser($this->Req->MDN, "HOTP");
-          SMSGW::SendSMS('Activation Key: ' . $SecretKey, $this->Req->MDN);
-          $this->Resp['MSG'] = "Please enter the Activation Key Sent to Mobile No. " . $this->Req->MDN;
-          $this->Resp['API'] = true;
-          //$this->Resp['DB']=$Profile[0];
-        } else {
-          $this->Resp['API'] = false;
-          $this->Resp['MSG'] = 'Mobile No.' . $this->Req->MDN . ' is not allowed to register!';
-        }
-        $fieldData['MobileNo'] = $this->Req->MDN;
-        $DB->insert(MySQL_Pre . 'SMS_Register', $fieldData);
-        break;
-
-      /**
-       * OTP Test: Test User OTP against Registration Data
-       * and if found valid update user credentials with new data
-       *
-       * Request:
-       *   JSONObject={"API":"OT",
+       *   JSONObject={"API":"US",
        *               "MDN":"9876543210",
-       *               "OTP":"123456"}
-       *
-       * Response:
-       *    JSONObject={"API":true,
-       *               "DB": {'KeyUpdated':1,
-       *                      "USER":{"UserName":"John Smith", TODO Send User Profile Data available at server.
-       *                              "Designation":"Operator",
-       *                              "eMailID":"jsmith@gmail.com"}
-       *                      }
-       *               "MSG":"Mobile No. 9876543210 is Registered Successfully.",
-       *               "ET":2.0987,
-       *               "ST":"Wed 20 Aug 08:31:23 PM"}
-       */
-      case 'OT':
-        $AuthUser = new AuthOTP(1);
-        if ($AuthUser->authenticateUser($this->Req->MDN, $this->Req->OTP)) {
-          $DB = new MySQLiDBHelper();
-
-          $this->Resp['DB']['KeyUpdated'] = $DB->where('MobileNo', $this->Req->MDN)
-            ->ddlQuery('Update ' . MySQL_Pre . 'SMS_Users Set UserData=TempData');
-
-          $DB->where('MobileNo', $this->Req->MDN);
-          $Profile                  = $DB->query('Select UserName, Designation, eMailID FROM ' . MySQL_Pre . 'SMS_Users');
-          $this->Resp['DB']['USER'] = $Profile[0];
-
-          $this->Resp['API'] = true;
-          $this->Resp['MSG'] = 'Mobile No. ' . $this->Req->MDN . ' is Registered Successfully!'
-            . ' Now you can start using NIC SMS Gateway for sending Group Messages.';
-        } else {
-          //$this->Resp['URL'] = $AuthUser->createURL($this->Req->MDN);
-          $this->Resp['DB']  = "Key: Not For Production"; //. $AuthUser->oath_hotp($AuthUser->getKey($this->Req->MDN), $this->Req->TC);
-          $this->Resp['API'] = false;
-          $this->Resp['MSG'] = 'Invalid OTP';
-        }
-        break;
-
-      /**
-       * All Groups: Retrieve All Groups
-       *
-       * Request:
-       *   JSONObject={"API":"AG",
-       *               "MDN":"9876543210",
+       *               "UID":"35",
        *               "OTP":"987654"}
        *
        * Response:
@@ -154,46 +98,18 @@ class AndroidAPI {
        *               "ET":2.0987,
        *               "ST":"Wed 20 Aug 08:31:23 PM"}
        */
-      case 'AG':
+      case 'US':
         $AuthUser = new AuthOTP();
         if ($AuthUser->authenticateUser($this->Req->MDN, $this->Req->OTP)) {
-          $this->Resp['DB']  = Group::getAllGroups();
+          $DB = new MySQLiDBHelper();
+          $DB->where('UserMapID', $this->Req->UID);
+          $Schemes           = $DB->get(MySQL_Pre . 'MPR_ViewWorkerSchemes');
+          $this->Resp['DB']  = $Schemes;
           $this->Resp['API'] = true;
-          $this->Resp['MSG'] = 'All Groups Loaded';
-        } else {
-          $this->Resp['API'] = false;
-          $this->Resp['MSG'] = 'Invalid OTP ' . $this->Req->OTP;
-        }
-        break;
-
-      /**
-       * Send SMS To a Group.
-       *
-       * Request:
-       *   JSONObject={"API":"SM",
-       *               "MDN":"9876543210",
-       *               "TXT":"Hello",
-       *               "GRP":"BDO"}
-       *
-       * Response:
-       *   JSONObject={"API":true,
-       *               "DB":"Return Message ID".
-       *               "MSG":"Message Sent",
-       *               "ET":2.0987,
-       *               "ST":"Wed 20 Aug 08:31:23 PM"}
-       */
-      case 'SM':
-        $AuthUser = new AuthOTP();
-        if ($AuthUser->authenticateUser($this->Req->MDN, $this->Req->OTP)) {
-          $Msg               = new Message();
-          $User              = new User($this->Req->MDN);
-          $Mid               = $Msg->createSMS($User, $this->Req->TXT, $this->Req->GRP);
-          $Contact           = new Contact();
-          $count             = $Contact->CountContactByGroup($this->Req->GRP);
-          $this->Resp['DB']  = $Mid;
-          $this->Resp['API'] = true;
-          $this->Resp['MSG'] = 'Message Sent to ' . $count
-            . ' Contacts of ' . $this->Req->GRP . ' Group';
+          $this->Resp['MSG'] = 'All Schemes Loaded';
+          $this->setExpiry(3600);
+          unset($DB);
+          unset($Schemes);
         } else {
           $this->Resp['API'] = false;
           $this->Resp['MSG'] = 'Invalid OTP ' . $this->Req->OTP;
@@ -210,11 +126,7 @@ class AndroidAPI {
        *
        * Response:
        *    JSONObject={"API":true,
-       *               "DB": {"UserName":"John Smith",
-       *                      "Designation":"Operator",
-       *                      "eMailID":"jsmith@gmail.com"
-       *                     }
-       *
+       *               "DB": {"UserMapID":"35"},
        *               "MSG":"Profile Downloaded Successfully.",
        *               "ET":2.0987,
        *               "ST":"Wed 20 Aug 08:31:23 PM"}
@@ -224,7 +136,7 @@ class AndroidAPI {
         if ($AuthUser->authenticateUser($this->Req->MDN, $this->Req->OTP)) {
           $DB = new MySQLiDBHelper();
           $DB->where('MobileNo', $this->Req->MDN);
-          $Profile          = $DB->query('Select UserName, Designation, eMailID FROM ' . MySQL_Pre . 'SMS_Users');
+          $Profile          = $DB->query('Select UserMapID FROM ' . MySQL_Pre . 'Users');
           $this->Resp['DB'] = $Profile[0];
 
           $this->Resp['API'] = true;
@@ -247,7 +159,7 @@ class AndroidAPI {
     }
   }
 
-  protected function sendResponse($Expiry=null) {
+  protected function sendResponse() {
     //$this->Resp['json'] = $this->Req; //TODO: Remove for Production
     $this->Resp['ET'] = time() - $this->Resp['ET'];
     $DateFormat       = 'D d M g:i:s A';
@@ -257,15 +169,7 @@ class AndroidAPI {
 
     header('Content-Type: application/json');
     header('Content-Length: ' . strlen($JsonResp));
-    /**
-     * Important: Tells volley not to cache the response
-     */
-    if($Expiry==null){
-      $Expires=time() - 3600;
-    } else {
-      $Expires=time() + $Expiry;
-    }
-    header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', $Expires));
+    header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', $this->getExpiry()));
     echo $JsonResp;
   }
 
